f0e127f1cfb8203ca2b7cf53e7ac86ea43650f27
aratushna <94103037+aratushna@users.noreply.github.com>
2023-03-23 22:43:24 +0200

    Update Dictionary.cs

--- Dictionary.cs
+++ Dictionary.cs
@@ -1,3 +1,5 @@
+using HashTable;
+using KeyValuePair = System.Collections.Generic.KeyValuePair;
 namespace Hashtable;
 
 /*Маючи зв'язний список ми можемо реалізувати вже і основну структуру - словник.
@@ -17,49 +19,99 @@ namespace Hashtable;
 public class Dictionary
 {
     private const int InitialSize = 10;
-    private List <LinkedList> buckets_10 = new List <LinkedList>(InitialSize);
-    private List<LinkedList> _buckets = new List <LinkedList>();
+    private List<LinkedList> buckets_10 = new List<LinkedList>(InitialSize);
+    private List<LinkedList> _buckets = new List<LinkedList>();
     private int num_buckets = 0;
-    private double load_factor = 0.7;
-    private int num_load_facktor = 0;
-        
+    private double load_factor = 0.85;
+    private int num_load_factor = 0;
+
     public void Add(string key, string value)
     {
 
-        if (num_buckets == 0)_buckets = buckets_10;
-        if ((num_buckets / _buckets.Count) >= load_factor) 
+        if (num_buckets == 0)
         {
-            _buckets.AddRange(buckets_10); 
-            num_load_facktor ++;
-            num_buckets = num_load_facktor * 10 - 1;  
+            for (int i = 0; i < buckets_10.Capacity; i++) buckets_10.Add(new LinkedList());
+            _buckets = buckets_10;
+        }
+        
+        if ( (num_buckets / _buckets.Count) >= load_factor )
+        {
+            _buckets.AddRange(buckets_10);
+            num_load_factor++;
+            num_buckets = num_load_factor * 10 - 1;
         }
 
-        int hash = CalculateHash(key);
-        var index = (hash % _buckets.Length) + (10 * num_load_facktor);  
-  
+        // int hash = CalculateHash(key);
+        long hash = CalculateHash(key);
+        // var index = hash % _buckets.Count + (10 * num_load_factor);
+        int index = Convert.ToInt32((hash % 10 + (10 * num_load_factor)));
 
+        /*
         var value = _buckets[index];
-
-
         if (value == null)
         {
             value = new LinkedList();
         }
         value.Add(new KeyValuePair(key, value));
-
-        num_buckets ++;
-    }
+        */
         
-
-
+        var new_KVP = new HashTable.KeyValuePair(key, value);
+        
+        if (_buckets[index] == null)    
+        {
+            var new_LL = new LinkedList();
+            new_LL.Add(new_KVP);
+            _buckets[index] = new_LL;
+        }
+        
+        _buckets[index].Add(new_KVP);
+        num_buckets++;
+    }
+    
     public void Remove(string key)
     {
-        int hash = CalculateHash(key);
-        int index = hash % _buckets.Length;
+        //int hash = CalculateHash(key);
+        long hash = CalculateHash(key);
+        int index = Convert.ToInt32(hash % _buckets.Count);
 
         if (_buckets[index] != null)
         {
             _buckets[index].RemoveByKey(key);
-        } 
-        num_buckets --;       
+        }
+        num_buckets--;
     }
+
+    
+    public string Get(string key)
+    {
+        //int hash = CalculateHash(key);
+        long hash = CalculateHash(key);
+        int index = Convert.ToInt32(hash % _buckets.Count);
+
+        if (_buckets[index] != null)
+        {
+            HashTable.KeyValuePair pair = _buckets[index].GetItemWithKey(key);
+            
+            if (pair != null)
+            {
+                return pair.Value;
+            }
+        }
+
+        return null;
+    }
+
+    
+    //private int CalculateHash(string key)
+    private long CalculateHash(string key)
+    {
+        //int hash = 0;
+        long hash = 0;
+        foreach (char c in key)
+        {
+            hash = hash * 13 + c;
+        }
+        hash = hash + key.Length;
+        return hash;
+    }
+}
