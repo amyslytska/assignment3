e1b83cbf4b8f3507d41b74f74cc6554c9216fc95
aratushna <94103037+aratushna@users.noreply.github.com>
2023-03-23 22:41:27 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace HashTable;
+﻿using Hashtable;
+
+namespace HashTable;
 using System;
 
 internal class Program
@@ -7,14 +9,17 @@ internal class Program
     {
         var dictionary = new Dictionary();
         var lines = File.ReadAllLines("C:/Users/annmy/RiderProjects/Hash Table/testDict.txt");
+        int i = 0;
         foreach (string line in lines)
         {
             string[] parts = line.Split(';');
             string key = parts[0].Trim();
             string value = parts[1].Trim();
             dictionary.Add(key, value);
+            if (i % 1000 == 0) Console.WriteLine("Number of loaded words " + i);
+            i++;
         }
-
+        
         Console.WriteLine("Enter a word:");
         string word = Console.ReadLine();
         string meaning = dictionary.Get(word);
